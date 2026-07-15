719508fc39d4cda5629df0cf5bce264684cb9942
ShoaibShokat03 <shoaibshokat03@gmail.com>
2026-07-15 14:23:28 +0500

    notice

--- app.config.js
+++ app.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     target_url: 'https://vulms.vu.edu.pk/', // Replace with your target URL
-    target_text: 'Check Your Datesheet',
-    //  target_text: 'Notice Board',
+    // target_text: 'Check Your Datesheet',
+     target_text: 'Notice Board',
     ntfy_topic: 'hamid-notify'
 };
